9ac1a88459b2b325cf4cb500ccac9796b353a059
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-08-20 21:35:14 -0700

    added the crateRow function and targeted the number of pages, posts, users and visits of the card in the main area of index.html

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,11 +1,9 @@
 //Selection
 const userNameInput = document.querySelector('#pageUserName');
 const loginBtn = document.querySelector('#btnLogin');
-
 //Events
 document.addEventListener('click',getUserName);
-
 //Function
 function getUserName(){
-    localStorage.setItem('userName',userNameInput.value);
+    sessionStorage.setItem('userName',userNameInput.value);
 }
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,13 @@
 //Selection
 const targetUserName = document.querySelector('#loggedUser');
+const numberOfPages = document.querySelector('#cardNumPages');
+const numberOfPosts = document.querySelector('#cardNumPosts');
+const numberOfUsers = document.querySelector('#cardNumUsers');
+const numberOfVisists = document.querySelector('#cardNumVisits');
+
 
 //Events
 document.addEventListener('DOMContentLoaded', onLoad);
-
 
 //Functions
 function onLoad(){
@@ -12,6 +16,19 @@ function onLoad(){
 }
 
 function setUserName(){
-    targetUserName.innerHTML = localStorage.getItem('userName');
+    targetUserName.classList.add('numbers');
+    targetUserName.innerHTML = sessionStorage.getItem('userName');
+}
 
+function createRow(a,b,c,d){
+    const table = document.querySelector('#latestUsersTable');
+    let row = table.insertRow(1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(4);
+    cell1.innerHTML = a;
+    cell2.innerHTML = b;
+    cell3.innerHTML = c;
+    cell4.innerHTML = d;
 }
