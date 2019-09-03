c63507f966604b0220cf0443d18381f4e27e669e
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-09-03 08:26:35 -0700

    added the ajaxCalls() and the setContainerArrays() on login.html

--- resources/js/login.js
+++ resources/js/login.js
@@ -5,5 +5,46 @@ const loginBtn = document.querySelector('#btnLogin');
 document.addEventListener('click',getUserName);
 //Function
 function getUserName(){
+    ajaxCalls();
     sessionStorage.setItem('userName',userNameInput.value);
+}
+
+function setContainerArrays(array){
+    let containerArray = [];
+    for (let item of array) {
+        //populates local users array with imported data
+        containerArray.push(item);
+    }
+    //stores array of user objects into local sotrage
+    localStorage.setItem('storedUsersArray', JSON.stringify(containerArray));
+}
+
+function ajaxCalls(){
+    //Ajax Request for registered Users
+    getData('GET', 'http://jsonplaceholder.typicode.com/users').then(function(data){
+        if(ajaxFlag == false){
+            let dataArray = JSON.parse(data);
+            setContainerArrays(dataArray);
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
+    //Ajax Request for total Posts in Site
+    getData('GET', 'https://jsonplaceholder.typicode.com/posts').then(function(data){
+        if(ajaxFlag == false){
+            let dataArray = JSON.parse(data);
+            setContainerArrays(dataArray);
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
+    //Ajax Request for total Pages in Site
+    getData('GET', './resources/jsonFiles/pages.json').then(function(data){
+        if(ajaxFlag == false){
+            let dataArray = JSON.parse(data);
+            setContainerArrays(dataArray);
+        }
+    }).catch(function(err){
+        console.log(err);
+    });
 }
