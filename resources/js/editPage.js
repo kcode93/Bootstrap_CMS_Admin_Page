206d01582c5f8aae04a7ae471d40332e274c483b
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-09-24 16:37:11 -0700

    added the setPublishedAwesomeIcon(), setTotalUsersCounter(), setTotalPagesCounter() and setTotalPostsCounter()

--- resources/js/editPage.js
+++ resources/js/editPage.js
@@ -20,3 +20,42 @@ function onLoad() {
     CKEDITOR.replace('editor1');
     setUserName();
 }
+
+function setUserName() {
+    //adds number class to span element and adds the user entered in logni.html
+    targetUserName.classList.add('numbers');
+    targetUserName.innerHTML = sessionStorage.getItem('userName');
+    setInitialValues();
+}
+
+function setPublishedAwesomeIcon(value) {
+    if (value == true) {
+        //returns CHECK awesome icon if page was published
+        return AWESOMEICONCHECK;
+    } else {
+        //returns X awesome icon if page was not published
+        return AWESOMEICONX;
+    }
+}
+
+function setTotalUsersCounter() {
+    let localTotalUsersArray = JSON.parse(localStorage.getItem("storedUsersArray") || []);
+    let totalSiteUsers = localTotalUsersArray.length;
+    //displays the total number of users in the site.
+    asideUserCounter.innerHTML = totalSiteUsers;
+}
+
+function setTotalPostsCounter() {
+    let localTotalPostsArray = JSON.parse(localStorage.getItem("storedPostsArray") || []);
+    let totalSitePosts = localTotalPostsArray.length;
+    //displays the total number of posts in the site.
+    asidePostsCounter.innerHTML = totalSitePosts;
+
+}
+
+function setTotalPagesCounter() {
+    let localTotalPagesArray = JSON.parse(localStorage.getItem("storedPagesArray") || []);
+    let totalSitePages = localTotalPagesArray.length;
+    //displays the total number of pages in the site.
+    asidePagesCounter.innerHTML = totalSitePages;
+}
