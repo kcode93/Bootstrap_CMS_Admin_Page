53dcb4873cc3924fb8e8674168abbfce6b9be665
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-09-24 16:34:49 -0700

    added the final selections for the editPage.js and added the event listeners for the onLoad() and the clearAllStorage(), also created the onLoad() that calls CKEDITOR() and setUserName()

--- resources/js/editPage.js
+++ resources/js/editPage.js
@@ -1,6 +1,22 @@
 //Selections
 const targetUserName = document.querySelector('#loggedUser');
+const logOut = document.querySelector('#logOut');
 const asideUserCounter = document.querySelector('#asideUserCounter');
 const asidePostsCounter = document.querySelector('#asidePostsCounter');
 const asidePagesCounter = document.querySelector('#asidePagesCounter');
-const logOut = document.querySelector('#logOut');
+const pageTitle = document.querySelector('#pageTitle');
+const pageBody = document.querySelector('#pageBody');
+const pagePublished = document.querySelector('#pagePublished');
+const pageTags = document.querySelector('#pageTags');
+const pageDescriptions = document.querySelector('#pageDes');
+
+//Events
+document.addEventListener('DOMContentLoaded', onLoad);
+logOut.addEventListener('click', clearAllStorage);
+
+//Functions
+function onLoad() {
+    //implements CKEditor
+    CKEDITOR.replace('editor1');
+    setUserName();
+}
