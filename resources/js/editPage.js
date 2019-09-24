337cbfef8d135e6d91e6710abe65531f1572d142
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-09-24 16:19:58 -0700

    tweaked the pages.js and added the selections needed for the editPage.js

---
+++ resources/js/editPage.js
@@ -0,0 +1,6 @@
+//Selections
+const targetUserName = document.querySelector('#loggedUser');
+const asideUserCounter = document.querySelector('#asideUserCounter');
+const asidePostsCounter = document.querySelector('#asidePostsCounter');
+const asidePagesCounter = document.querySelector('#asidePagesCounter');
+const logOut = document.querySelector('#logOut');
